bfea87475a41a3253d60d1365b22d203028d056b
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2025-06-02 09:52:58 +0200

    Fix tmux

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -71,6 +71,17 @@ let name = "Edoardo Debenedetti";
         let carapace_completer = {|spans|
             carapace $spans.0 nushell ...$spans | from json
         }
+        let zoxide_completer = {|spans|
+            $spans | skip 1 | zoxide query -l ...$in | lines | where {|x| $x != $env.PWD}
+        }
+
+        let multiple_completers = {|spans|
+            match $spans.0 {
+                z => $zoxide_completer
+                _ => $carapace_completer
+            } | do $in $spans
+        }
+
         $env.config = {
             show_banner: false,
             completions: {
@@ -83,8 +94,8 @@ let name = "Edoardo Debenedetti";
                 enable: true
                 # set to lower can improve completion performance at the cost of omitting some options
                 max_results: 100
-                completer: $carapace_completer # check 'carapace_completer'
-                }
+                completer: $multiple_completers # check 'carapace_completer'
+              }
             }
         }
         $env.PATH = ($env.PATH | split row (char esep) | append $"($env.HOME)/.local/bin")
@@ -95,8 +106,18 @@ let name = "Edoardo Debenedetti";
         nano = "hx";
         };
   };
-  carapace.enable = true;
-  carapace.enableNushellIntegration = true;
+
+  carapace = {
+    enable = true;
+    enableNushellIntegration = true;
+  };
+
+  zoxide = {
+    enable = true;
+    enableNushellIntegration = true;
+    enableZshIntegration = true;
+  };
+
 
   git = {
     enable = true;
@@ -306,9 +327,8 @@ let name = "Edoardo Debenedetti";
 
   tmux = {
     enable = true;
+    shell = "${pkgs.nushell}/bin/nu";
     plugins = with pkgs.tmuxPlugins; [
-      vim-tmux-navigator
-      sensible
       yank
       prefix-highlight
       {
@@ -336,55 +356,13 @@ let name = "Edoardo Debenedetti";
         '';
       }
     ];
-    terminal = "screen-256color";
-    prefix = "C-x";
     escapeTime = 10;
     historyLimit = 50000;
     extraConfig = ''
-      # Remove Vim mode delays
-      set -g focus-events on
-
-      # Enable full mouse support
-      set -g mouse on
-
       # -----------------------------------------------------------------------------
       # Key bindings
       # -----------------------------------------------------------------------------
 
-      # Unbind default keys
-      unbind C-b
-      unbind '"'
-      unbind %
-
-      # Split panes, vertical or horizontal
-      bind-key x split-window -v
-      bind-key v split-window -h
-
-      # Move around panes with vim-like bindings (h,j,k,l)
-      bind-key -n M-k select-pane -U
-      bind-key -n M-h select-pane -L
-      bind-key -n M-j select-pane -D
-      bind-key -n M-l select-pane -R
-
-      # Smart pane switching with awareness of Vim splits.
-      # This is copy paste from https://github.com/christoomey/vim-tmux-navigator
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
-        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
-      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
-      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
-      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-        "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-        "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
-
-      bind-key -T copy-mode-vi 'C-h' select-pane -L
-      bind-key -T copy-mode-vi 'C-j' select-pane -D
-      bind-key -T copy-mode-vi 'C-k' select-pane -U
-      bind-key -T copy-mode-vi 'C-l' select-pane -R
-      bind-key -T copy-mode-vi 'C-\' select-pane -l
       '';
     };
 
